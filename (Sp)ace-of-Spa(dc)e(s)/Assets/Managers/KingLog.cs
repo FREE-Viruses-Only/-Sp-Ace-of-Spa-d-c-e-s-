5b86809025e47e003ef6ea7b4df4869f20d26529
FREE-Viruses-Only <vvhoknows@gmail.com>
2023-05-23 10:17:33 -0400

    deleted whole projec smooth crazy corn

--- (Sp)ace-of-Spa(dc)e(s)/Assets/Managers/KingLog.cs
+++ (Sp)ace-of-Spa(dc)e(s)/Assets/Managers/KingLog.cs
@@ -10,21 +10,81 @@ public class KingLog : MonoBehaviour
 {
     public static KingLog Instance;
     [SerializeField] private TextMeshProUGUI Logs;
+
+    private static int logCount = 0;
+    private static string transmogrify = "Booting";
+
+    private static List<string> theeseOnes = new List<string>();
+    private static string bigString;
+    private static int depth = -20;
+    
+    static Dictionary<int, string> homestuck = new Dictionary<int, string> { };
+
     //  private bool ienumeratorThing = false;
     // MAKE DICTIONARY FOR LOG
 
     void Awake()
     {
         Instance = this;
+        Log(transmogrify);
     }
 
     public void Log(string msg)
     {
-        Logs.text = Logs.text + "\n \n ************************************* \n" + msg;
-    }
+        //  Logs.text = Logs.text + "\n \n ************************************* \n" + msg;
+        transmogrify =  $"\n [{logCount}] ************************************* \n" + msg;
 
-    public void Update()
+        homestuck.Add(logCount, transmogrify);
+
+
+        logCount += 1;
+
+        depth++;
+
+        POST();
+    }
+    /*
+    public void Log(string msg)
     {
+        //  Logs.text = Logs.text + "\n \n ************************************* \n" + msg;
+        homestuck.Add(logCount, transmogrify);
+
+        transmogrify = msg;
+
+        logCount += 1;
+
+        depth++;
+
+        POST();
+    }
+    */
+    public void POST()
+    {
+        theeseOnes.Clear();
+        Logs.text = "";
+        for (int i = 0; i < 20; i++)
+        {
+            if (i >= homestuck.Count)
+            {
+                break;
+            }
+            if (depth >= 0)
+            {
+                theeseOnes.Add(homestuck[i + depth].ToString());
+
+            }
+            else
+            {
+                theeseOnes.Add(homestuck[i].ToString());
+            }
+
+        }
+
+        foreach (string entry in theeseOnes)
+        {
+            Logs.text += entry;
+        }
+
         if (Logs.textInfo.lineCount > 200)
         {
 
